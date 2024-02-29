9534ff03e3210c56500bd2f3b96e1769ba92aa5e
Vikram Singh <vikram.singh@johnabbott.qc.ca>
2024-02-28 21:12:29 -0500

    Quartz sync: Feb 28, 2024, 9:12 PM

---
+++ content/index.md
@@ -0,0 +1,5 @@
+---
+title: Welcome to my Huge Mess!
+---
+
+This is a dump of all my research so far. It's very messy. I should buy a broom or something 🧹
--- quartz.config.ts
+++ quartz.config.ts
@@ -3,14 +3,14 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 Vik's Digital Garden",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "mtp.singh.gg",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
